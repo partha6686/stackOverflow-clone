d9b88222914e3ee943b149cb4d1cd11065834101
Partha Sarathi Praharaj <parthasarathipraharaj@Parthas-MacBook-Pro.local>
2024-01-13 23:44:24 +0530

    Sidebar complete

--- src/components/shared/SideBar/index.jsx
+++ src/components/shared/SideBar/index.jsx
@@ -1,32 +1,55 @@
-import React from 'react'
-import styles from "./SideBar.module.css"
-import { AiOutlineHome, AiOutlineGlobal, AiOutlineTeam, AiOutlineDown } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import styles from "./SideBar.module.css";
+import {
+  AiOutlineHome,
+  AiOutlineGlobal,
+  AiOutlineTeam,
+  AiOutlineDown,
+  AiOutlineUp,
+} from "react-icons/ai";
+import { HiOutlineBriefcase } from "react-icons/hi2";
+import { useLocation, Link } from "react-router-dom";
 
 const SideBar = () => {
+  let location = useLocation();
+  const [open, setOpen] = useState(true);
+  console.log("location", location.pathname);
   return (
-    <div>
-      <div>
-        <Link to="/" >
-          <AiOutlineHome />
+    <div className={styles.sidebar_wrapper}>
+      <div className={styles.sidebar}>
+        <Link to="/" className={location.pathname == "/" ? styles.active_item : styles.sidebar_item}>
+          <AiOutlineHome size={20} />
           <p>Home</p>
         </Link>
-        <div>
-        <AiOutlineGlobal />
-        <p>Public</p>
-        <AiOutlineDown />
+        <div className={styles.sidebar_item} onClick={() => setOpen(!open)}>
+          <AiOutlineGlobal size={20} />
+          <p>Public</p>
+          {open ? <AiOutlineUp className={styles.icon_right} /> : <AiOutlineDown className={styles.icon_right} />}
         </div>
-        <Link to="/" >
-          <AiOutlineTeam />
+        {open && (
+          <div className={styles.sidebar_internal}>
+            <Link to="/questions" className={location.pathname == "/questions" && styles.active_item}>
+              <p>Questions</p>
+            </Link>
+            <Link to="/tags" className={location.pathname == "/tags" && styles.active_item}>
+              <p>Tags</p>
+            </Link>
+            <Link to="/users" className={location.pathname == "/users" && styles.active_item}>
+              <p>Users</p>
+            </Link>
+          </div>
+        )}
+        <Link to="/collectives" className={location.pathname == "/collectives" ? styles.active_item : styles.sidebar_item}>
+          <AiOutlineTeam size={20} />
           <p>Collectives</p>
         </Link>
-        <Link to="/" >
-        <AiOutlineGlobal />
+        <Link to="/jobs" className={location.pathname == "/jobs" ? styles.active_item : styles.sidebar_item}>
+          <HiOutlineBriefcase size={20} />
           <p>Jobs</p>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SideBar;
